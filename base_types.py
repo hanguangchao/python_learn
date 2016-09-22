c1bc6689f9367877ead014133a8a9a65e0d471be
hanguangchao <hanguangchao@gmail.com>
2016-09-23 01:08:42 +0800

    -

--- base_types.py
+++ base_types.py
@@ -2,6 +2,8 @@
 
 import math
 
+import fractions
+
 print dir(math)
 
 print math.pi
@@ -9,4 +11,25 @@ print math.pi
 print round(math.pi)
 print round(math.pi, 4)
 
-print help(round)
+# help(round)
+
+print float(1)
+print int(-1.123)
+
+
+
+def is_it_true(anything):
+    if anything:
+        print("yes, it's true")
+    else:
+        print("no, it's false")
+
+
+if __name__ == '__main__':
+    is_it_true(1)
+    is_it_true(-1)
+    is_it_true(0) 
+    is_it_true(0.1)
+    is_it_true([])
+
+    is_it_true(fractions.Fraction(1, 2))    
