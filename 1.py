3ebc127b32a1b44398383f3d90faa233e38a3707
hanguangchao <hanguangchao@gmail.com>
2017-04-25 14:03:16 +0000

    -

---
+++ 1.py
@@ -0,0 +1,44 @@
+# -*- coding=utf-8 -*-
+
+from __future__ import division
+
+print 1 / 2
+print 1 // 2
+
+
+print 1 / 2 
+print 1 % 2
+
+
+'''
+乘方
+'''
+
+print 2 ** 3
+print -3 ** 2 # 等同于 -(3**2)
+print (-3) * 2
+
+'''
+十六进制
+'''
+print 0xaf
+print 0xff
+
+'''
+八进制
+'''
+print 010
+
+
+# s = input('提示：')
+# print s
+
+
+print pow(2, 3)
+print pow(2, 32, 3)
+
+
+import math
+
+print math.floor(32.123)
+
--- use-list.py
+++ use-list.py
@@ -1,44 +1,104 @@
 # -*- coding=utf-8 -*-
 
-from __future__ import division
-
-print 1 / 2
-print 1 // 2
-
-
-print 1 / 2 
-print 1 % 2
-
-
 '''
-乘方
+list
 '''
 
-print 2 ** 3
-print -3 ** 2 # 等同于 -(3**2)
-print (-3) * 2
+mylist = [1, 2, 3]
+mylist.append(4)
+mylist.append([4, 5, 6])
 
-'''
-十六进制
-'''
-print 0xaf
-print 0xff
-
-'''
-八进制
-'''
-print 010
+print mylist
 
 
-# s = input('提示：')
-# print s
+lista = [1, 2, 3]
+lista[1] = 5
+print lista
+
+del lista[1]
+# del lista[3]
+print lista
+
+# 分片赋值
+l = list('Perl')
+l[2:] = 'ar'
+print l
+l[1:] = 'ython'
+print l
+
+number = [1, 5]
+number[1:1] = [2, 3, 4]
+print number
+number[1:4] = []
+print number
+
+"""
+列表方法
+list.append()
+list.count()
+list.extend()
+"""
+
+b = ['a', 'b', 'c', 'd', 'a', 'c', 'c']
+b.append('f')
+b.append(['g', 'h'])
+b.append([1, 2, 3])
+print b
+print b.count('a')
+print b.count('c')
+print b.count('e')
 
 
-print pow(2, 3)
-print pow(2, 32, 3)
+a = [1, 2, 3]
+b = [4, 5, 6]
+print 'a::' , a
+print 'a+b::', a+b 
+print a.index(3)
+
+c = [1, 2, 3]
+d = [4, 5, 6]
+c.extend(d)
+print c
+
+c.insert(3, 'aaa')
+print c
+c.insert(-1, 'ddd')
+print c
+
+c.pop()
+print c
+
+c.remove(1)
+print c
+c.reverse()
+print c
+
+x = [4, 6, 3 ,2,7, 5]
+x.sort()
+print x
+
+print x[:]
+y = x
+
+print y
+y.sort()
+print y
+
+z = sorted(x)
+print z  
 
 
-import math
+"""
+元组
+"""
+print 1,
+print (1,)
+print 8*(1+2,)
 
-print math.floor(32.123)
+x = [1, 2, 3]
+y = tuple(x)
+print x, y 
+
+
+
 
