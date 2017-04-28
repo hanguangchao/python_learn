c0936ad5548fa57256c2f698fb930bb409d8a571
hanguangchao <hanguangchao@gmail.com>
2017-04-28 06:02:13 +0000

    alien_invasion

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import sys
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -17,9 +18,12 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
 
+    bullets = Group()
+
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
