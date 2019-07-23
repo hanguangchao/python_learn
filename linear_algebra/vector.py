1f30bfdfd3441a8a7599cba5bf9915441afd9fe1
hanguangchao <hanguangchao@gmail.com>
2019-07-23 15:43:33 +0800

    -

--- linear_algebra/vector.py
+++ linear_algebra/vector.py
@@ -6,11 +6,52 @@ Created on Tue Jul 23 12:24:56 2019
 @author: han
 """
 
+import math
+
+
 class Vector:
-    #构造方法，传进来的是一个lst数组
+    #构造方法，传进来的是一个lst数组  
     def __init__(self,lst):
         self._values=lst
+        
+    def __add__(self, another):
+        """向量加法，返回结果向量"""
+        assert len(self) == len(another), \
+            "Error in adding. Length of vectors must be same."
 
+        # return Vector([a + b for a, b in zip(self._values, another._values)])
+        return Vector([a + b for a, b in zip(self, another)])
+
+    def __sub__(self, another):
+        """向量减法，返回结果向量"""
+        assert len(self) == len(another), \
+            "Error in subtracting. Length of vectors must be same."
+            
+        return Vector([a - b for a, b in zip(self, another)])
+
+    def __mul__(self, k):
+        """返回数量乘法的结果向量：self * k"""
+        return Vector([k * e for e in self])
+
+    def __rmul__(self, k):
+        """返回数量乘法的结果向量：k * self"""
+        return self * k
+    
+    def __truediv__(self, k):
+        """返回数量除法的结果向量：self / k"""
+        return (1 / k) * self
+
+    def __pos__(self):
+        """返回向量取正的结果向量"""
+        return 1 * self
+
+    def __neg__(self):
+        """返回向量取负的结果向量"""
+        return -1 * self
+
+    def __iter__(self):
+        """返回向量的迭代器"""
+        return self._values.__iter__()
     #供系统调用的魔法方法
     def __repr__(self):
         return "Vector({})".format(self._values)
@@ -29,4 +70,18 @@ class Vector:
         """取向量的第index个元素"""
         return self._values[index]
     
+
+    
+    @classmethod
+    def zero(cls, dim):
+        """返回一个dim维的零向量"""
+        return cls([0] * dim)
+    
+    def norm(self):
+        """返回向量的模:向量的长度"""
+        return math.sqrt(sum(e**2 for e in self))
+    
+    def normalize(self):
+        """返回向量的单位向量"""
+        return Vector(self._values) / self.norm()
     
--- linear_algebra/vector_main.py
+++ linear_algebra/vector_main.py
@@ -14,3 +14,32 @@ if __name__ == "__main__":
     print(vec)
     print(len(vec))
     print("vec[0] = {}, vec[1] = {}".format(vec[0], vec[1]))
+    
+    vec2 = Vector([3, 1])
+    print("{} + {} = {}".format(vec, vec2, vec + vec2))
+    print("{} - {} = {}".format(vec, vec2, vec - vec2))
+    
+    print("{} * {} = {}".format(vec, 3, vec * 3))
+    print("{} * {} = {}".format(3, vec, 3 * vec))
+    
+    print("+{} = {}".format(vec, +vec))
+    print("-{} = {}".format(vec, -vec))
+    
+    
+    z = Vector.zero(2)
+    print(z)
+    print("{} + {} = {}".format(vec, z, vec + z))
+    
+    zero2 = Vector.zero(2)
+    print(zero2)
+    print("{} + {} = {}".format(vec, zero2, vec + zero2))
+
+    print("norm({}) = {}".format(vec, vec.norm()))
+    print("norm({}) = {}".format(vec2, vec2.norm()))
+    print("norm({}) = {}".format(zero2, zero2.norm()))
+
+    print("normalize {} is {}".format(vec, vec.normalize()))
+    print(vec.normalize().norm())
+
+    print("normalize {} is {}".format(vec2, vec2.normalize()))
+    print(vec2.normalize().norm())
